49f7ea75120651c5094b58a8642205dabe68d248
黒瀬友太 <kuroseyuuta@mba.toshima.ne.jp>
2019-10-16 07:00:50 +0900

    Variables

--- src/playground.js
+++ src/playground.js
@@ -5,4 +5,19 @@
 
 export default function() {
   console.log("Hello World")
+
+  debugger
+  var firstName = "Kurose" //string
+  var age = 28 //number
+  var isMale = true //boolean
+  var dontKnoe = null //unknow value
+  var a
+
+  var person = {
+    name: "Kurose",
+    lastName: "Yuta"
+  }
+
+  // Create all types of variables of your choice
+  // Check their types with "typeof"
 }
